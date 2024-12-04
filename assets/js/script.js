ab5db607c24eddb7c448ba1f6778117892582559
Ben Williams <codebencode@icloud.com>
2024-12-04 14:05:27 +0000

    Add keydown function and save browser data

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,15 @@ const notesContainer = document.querySelector('.notes-container'); // selects th
 const createBtn = document.querySelector('.btn'); // selects the btn class on the button
 let notes = document.querySelectorAll('.input-box'); // selects all the notes and everything inside the box
 
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem('notes');
+}
+showNotes();
+
+function updateStorage() { // this updates the browser to allow the data to be stored
+    localStorage.setItem('notes', notesContainer.innerHTML);
+}
+
 createBtn.addEventListener('click',()=> { // when button is clicked it will run this function
     let inputBox = document.createElement('p'); // creates a p
     let img = document.createElement('img'); // creates an image
@@ -13,6 +22,22 @@ createBtn.addEventListener('click',()=> { // when button is clicked it will run 
 
 notesContainer.addEventListener('click', function(e) { // e is event object
     if (e.target.tagName === 'IMG') { // e.target is the element that was clicked - IMG is the imgage which checked if the image was clicked
-        e.target.parentElement.remove(); // if the image was clciked - remove the paragragh text(the parent)
+        e.target.parentElement.remove();// if the image was clciked - remove the paragragh text(the parent)
+        updateStorage(); 
+    }
+    else if (e.target.tagName === 'P'){
+        notes = document.querySelectorAll('.input-box')
+        notes.forEach(nt => {
+            nt.onkeyup = function() {
+                updateStorage();
+            }
+        })
+    }
+})
+
+document.addEventListener('keydown', event => { // When enter is clicked it will add a line break and prevent enter from submitting (it's default value)
+    if (event.key === 'Enter') {
+        document.execCommand('insertLineBreak');
+        event.preventDefault()
     }
 })
